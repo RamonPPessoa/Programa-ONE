c43992c2439595d2b7408ddf075c2b93bd5d1fc2
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2023-07-03 16:01:42 -0400

    Programação Orietado a Objetos

---
+++ Java_OO/bytebank/src/main/java/org/example/Conta.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Conta {
+    double saldo;
+    int agencia;
+    int numero;
+    String titular;
+}
---
+++ Java_OO/bytebank/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Conta conta1 = new Conta();
+        conta1.saldo = 350;
+        System.out.println(conta1.saldo);
+    }
+}
