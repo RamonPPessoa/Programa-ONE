5b3bc26396c0453b893e85fb1a441d0406f12d18
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2023-07-12 18:32:31 -0400

    Analisando referência null

---
+++ Java_OO/bytebank_composto/src/main/java/org/example/ContaSemCliente.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class ContaSemCliente {
+    public static void main(String[] args) {
+        Conta contaPedro =  new Conta();
+        System.out.println(contaPedro.saldo);
+
+        contaPedro.titular = new Cliente();
+        System.out.println(contaPedro.titular);
+
+
+        contaPedro.titular.nome = " Pedro";
+        System.out.println(contaPedro.titular.nome);
+    }
+}
