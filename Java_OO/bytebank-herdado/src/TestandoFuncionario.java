a1dae5f9c007eeef8df0c69084db823b363bb4d3
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2023-07-28 09:29:09 -0400

    Utilizando polimorfismo,heranca e interfaces

---
+++ Java_OO/bytebank-herdado/src/TestandoFuncionario.java
@@ -0,0 +1,16 @@
+
+public class TestandoFuncionario {
+
+	public static void main(String[] args) {
+		 Funcionario fun1 = new Funcionario();
+		 
+		 fun1.setNome("Paula Silva");
+		 fun1.setCpf("2222555223");
+		 fun1.setSalario(2500.00);
+		 
+		System.out.println(fun1.getNome());
+		System.out.println(fun1.getBonificacao());
+
+	}
+
+}
