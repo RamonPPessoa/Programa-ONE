12c5f871c2fa0dd38fe8dd9ce0fba8eb5dc66c69
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2023-08-07 17:19:19 -0400

    Trabalhando com classes abstratas

--- Java_OO/bytebank-herdado/src/TesteReferencia.java
+++ Java_OO/bytebank-herdado/src/TesteReferencia.java
@@ -4,12 +4,10 @@ public class TesteReferencia {
 	public static void main(String[] args) {
 		
 		Gerente gen1 = new Gerente();
-		
 		gen1.setNome("Paula");
 		gen1.setSalario(6000);
 		
-		Funcionario f1 = new Funcionario();
-		f1.setSalario(3000);
+		
 		
 		Funcionario ev = new EditorDeVideo();		
 		ev.setSalario(2000);
@@ -17,7 +15,6 @@ public class TesteReferencia {
 		
 		ControleBonificacao controle = new ControleBonificacao();
 		controle.registra(gen1);
-		//controle.registra(f1);
 		//controle.registra(ev);
 		
 		System.out.println(controle.getSoma());
