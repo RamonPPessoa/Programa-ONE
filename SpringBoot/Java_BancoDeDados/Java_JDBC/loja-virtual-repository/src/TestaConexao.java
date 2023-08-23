ef1330c370d1a9c3760293745047a9fa46d04cfc
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2023-08-23 09:53:49 -0400

    Formação Java e Spring Boot,desenvolvendo aplicações web com java,programando com acesso ao banco de dados MySQL e JPA, e desenvolvendo uma API Rest com o framework Spring Boot

---
+++ SpringBoot/Java_BancoDeDados/Java_JDBC/loja-virtual-repository/src/TestaConexao.java
@@ -0,0 +1,17 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestaConexao {
+
+	public static void main(String[] args)throws SQLException { 
+	
+		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC","root","Rhamon071147!");
+		
+		System.out.println("Conexão realizada com sucesso!");
+		System.out.println("Fechando conexão!");
+		
+		connection.close();
+	}
+
+}
