9a6d583ea06984ff6218b0a83499969558c2cb90
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2023-08-24 16:50:37 -0400

    Remoção de dados

--- SpringBoot/Java_BancoDeDados/Java_JDBC/loja-virtual-repository/src/TesteRemocao.java
+++ SpringBoot/Java_BancoDeDados/Java_JDBC/loja-virtual-repository/src/TesteRemocao.java
@@ -1,4 +1,22 @@
 
-public class TesteRemocao {
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+public class TesteRemocao {
+	
+	public static void main(String[] args) throws SQLException{
+
+	ConnectionFactory factory = new ConnectionFactory();
+	Connection connection = factory.recuperaConexao();
+	
+	Statement stm = connection.createStatement();
+	stm.execute("DELETE FROM PRODUTO WHERE ID > 1"); 
+	
+Integer linhasModificadas =	stm.getUpdateCount();
+
+System.out.println("LInhas modificadas: "  + linhasModificadas);
+	
+	
+}
 }
