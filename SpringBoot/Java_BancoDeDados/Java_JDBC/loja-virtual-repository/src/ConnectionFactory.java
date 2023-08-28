5a6f27257cd0971b20b3d9180ba22e83262aa17a
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2023-08-28 19:09:23 -0400

    Utilizando Jars criando e testando o pool de conexões

--- SpringBoot/Java_BancoDeDados/Java_JDBC/loja-virtual-repository/src/ConnectionFactory.java
+++ SpringBoot/Java_BancoDeDados/Java_JDBC/loja-virtual-repository/src/ConnectionFactory.java
@@ -1,14 +1,26 @@
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public class ConnectionFactory {
 	
-	public Connection recuperaConexao()throws SQLException {
-		return DriverManager
-				.getConnection
-				("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC","root","Rhamon071147!");
+	public DataSource dataSource;
+	
+	public ConnectionFactory() {
+		ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+		comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC");
+		comboPooledDataSource.setUser("root");
+		comboPooledDataSource.setPassword("Rhamon071147!");
 		
+		comboPooledDataSource.setMaxPoolSize(15);
+		
+		this.dataSource = comboPooledDataSource;
+	}
+	public Connection recuperaConexao()throws SQLException {
+		return this.dataSource.getConnection();
 		
 		
 	}
