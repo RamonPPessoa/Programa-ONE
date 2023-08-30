59a5fc3a084e06ec588782fb8226f5536516982f
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2023-08-30 07:15:21 -0400

    Inserindo com Statement

---
+++ SpringBoot/Java_BancoDeDados/Java_JDBC/loja-virtual-repository/src/br/com/alura/one/TestaInsercao.java
@@ -0,0 +1,27 @@
+package br.com.alura.one;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+
+public class TestaInsercao {
+
+	public static void main(String[] args)throws SQLException {
+	
+		ConnectionFactory factory = new ConnectionFactory();
+		Connection connection = factory.recuperarConexao();
+		
+		Statement stm = connection.createStatement();
+		stm.execute("INSERT INTO PRODUTO(nome,descricao)VALUES('celular','Celular Samsung A 01')"
+					,Statement.RETURN_GENERATED_KEYS); 
+		
+		ResultSet rst = stm.getGeneratedKeys();
+		while(rst.next()) {
+			Integer id = rst.getInt(1);
+			System.out.println(" O id criado foi o: " + id);
+		}
+
+	}
+
+}
