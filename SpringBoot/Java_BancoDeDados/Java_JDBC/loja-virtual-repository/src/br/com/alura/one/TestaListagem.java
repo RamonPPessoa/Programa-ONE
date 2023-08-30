f7dc8b4192e223c35eceb27555be442e93f4813e
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2023-08-30 06:01:16 -0400

    Criando listagem com Statement

---
+++ SpringBoot/Java_BancoDeDados/Java_JDBC/loja-virtual-repository/src/br/com/alura/one/TestaListagem.java
@@ -0,0 +1,29 @@
+package br.com.alura.one;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestaListagem {
+
+	public static void main(String[] args) throws SQLException {
+		Connection connection =
+				DriverManager.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC", "root", "Rhamon071147!");
+
+		Statement stm = connection.createStatement();
+		boolean resultado = stm.execute("SELECT * FROM PRODUTO");
+
+		ResultSet rst = stm.getResultSet();
+		while(rst.next()) {
+			int id = rst.getInt("id");
+			String nome = rst.getString("nome");
+			String descricao = rst.getString("descricao");
+			System.out.println(id);
+			System.out.println(nome);
+			System.out.println(descricao);
+		}
+	}
+	
+}
