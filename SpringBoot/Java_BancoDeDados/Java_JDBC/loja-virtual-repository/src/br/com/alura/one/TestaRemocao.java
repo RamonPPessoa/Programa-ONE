0493696c36527856249f5a2a2f72a8fc16066f08
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2023-08-31 16:10:46 -0400

    Atualizando o arquivo TestaRemocao.java

--- SpringBoot/Java_BancoDeDados/Java_JDBC/loja-virtual-repository/src/br/com/alura/one/TestaRemocao.java
+++ SpringBoot/Java_BancoDeDados/Java_JDBC/loja-virtual-repository/src/br/com/alura/one/TestaRemocao.java
@@ -1,5 +1,25 @@
 package br.com.alura.one;
 
-public class TestaRemocao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+
+public class TestaRemocao {
+	
+	public static void main(String[] args) throws SQLException{
+
+	ConnectionFactory factory = new ConnectionFactory();
+	Connection connection = factory.recuperarConexao();
+	
+	PreparedStatement stm = connection.prepareStatement("DELETE FROM PRODUTO WHERE ID > ?");
+	stm.setInt(1,2);
+	stm.execute(); 
+	
+Integer linhasModificadas =	stm.getUpdateCount();
+
+System.out.println("LInhas que foram modificadas: "  + linhasModificadas);
+	
+	
+}
 }
