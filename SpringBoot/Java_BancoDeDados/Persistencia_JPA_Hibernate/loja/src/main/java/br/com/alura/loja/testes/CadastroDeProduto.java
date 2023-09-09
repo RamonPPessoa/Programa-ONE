fa4896d4a021a1941211abbe361b3f4272e669fb
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2023-09-09 07:24:47 -0400

    Persistência com JPA:Hibernate

--- SpringBoot/Java_BancoDeDados/Persistencia_JPA_Hibernate/loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ SpringBoot/Java_BancoDeDados/Persistencia_JPA_Hibernate/loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -1,6 +1,7 @@
 package br.com.alura.loja.testes;
 
 import br.com.alura.loja.dao.ProdutoDao;
+import br.com.alura.loja.modelo.Categoria;
 import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.util.JPAUtil;
 
@@ -12,11 +13,7 @@ import java.math.BigDecimal;
 public class CadastroDeProduto {
 
     public static void main(String[] args) {
-        Produto celular = new Produto();
-
-        celular.setNome("Xiaomi Redmi");
-        celular.setDescricao("Muito legal");
-        celular.setPreco(new BigDecimal("800"));
+        Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), Categoria.CELULARES);
 
         EntityManager em = JPAUtil.getEntityManager();
         ProdutoDao dao = new ProdutoDao(em);
