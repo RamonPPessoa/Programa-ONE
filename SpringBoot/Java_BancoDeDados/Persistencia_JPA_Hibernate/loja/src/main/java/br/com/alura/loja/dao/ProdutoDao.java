24f91373518c37c74cd7bb63969d165f2137e5a2
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2023-09-09 17:42:37 -0400

    Consultas com filtro:Buscando por nome

--- SpringBoot/Java_BancoDeDados/Persistencia_JPA_Hibernate/loja/src/main/java/br/com/alura/loja/dao/ProdutoDao.java
+++ SpringBoot/Java_BancoDeDados/Persistencia_JPA_Hibernate/loja/src/main/java/br/com/alura/loja/dao/ProdutoDao.java
@@ -12,17 +12,19 @@ public class ProdutoDao {
 
         this.em = em;
     }
+
     public void cadastrar(Produto produto) {
 
         this.em.persist(produto);
     }
+
     public void remover(Produto produto) {
         produto = em.merge(produto);
         this.em.remove(produto);
     }
 
     public Produto buscarPorId(Long id) {
-    return em.find(Produto.class,id);
+        return em.find(Produto.class, id);
     }
 
     public List<Produto> buscarTodos() {
@@ -30,6 +32,14 @@ public class ProdutoDao {
         return em.createQuery(jpql, Produto.class).getResultList();
     }
 
+    public List<Produto> buscarPorNome(String nome) {
+        String jpql = "SELECT p FROM Produto p WHERE p.nome = :nome";
+        return em.createQuery(jpql, Produto.class)
+                .setParameter("nome", nome)
+                .getResultList();
+
+
+    }
 }
 
 
