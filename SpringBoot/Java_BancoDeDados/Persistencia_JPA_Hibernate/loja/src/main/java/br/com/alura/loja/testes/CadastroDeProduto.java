eb19dacb5e2be8252a65e756171cb42294c1cff6
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2023-09-09 21:03:40 -0400

    Limitando os dados em uma consulta

--- SpringBoot/Java_BancoDeDados/Persistencia_JPA_Hibernate/loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ SpringBoot/Java_BancoDeDados/Persistencia_JPA_Hibernate/loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -24,6 +24,13 @@ public class CadastroDeProduto {
         List<Produto> todos = produtoDao.buscarPorNomeDaCategoria("CELULARES");
         todos.forEach(p2 -> System.out.println(p2.getNome()));
 
+        BigDecimal precoDoProduto = produtoDao.buscarPrecoDoProdutoComNome("Xiaomi Redmi");
+        System.out.println("Preco do Produto: " +precoDoProduto);
+
+
+
+
+
 
 
     }
